ad79ceefe92a438918ec07116809bedf25731be8
Anatoli <anatoli.tselms@myy.haaga-helia.fi>
2022-02-17 19:00:59 +0200

    first commit

--- src/main/java/com/example/bookStore/web/bookController.java
+++ src/main/java/com/example/bookStore/web/bookController.java
@@ -1,15 +1,23 @@
 package com.example.bookStore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.bookStore.BookRepository;
+
+
 
 @Controller
 public class bookController {
+	@Autowired
+	private BookRepository repository;
 
-	@RequestMapping("/index")
-	public String viewBooks() {
-		
-		return "index";
+	@RequestMapping("/booklist")
+	public String viewBooks(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
 	
