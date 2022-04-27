07d27d85b299810843775e364e2d83628291345d
IntoKanto <anatoli.tselms@myy.haaga-helia.fi>
2022-04-27 21:53:34 +0300

    Rest, bookid=4&5

--- src/main/java/com/example/bookStore/web/bookController.java
+++ src/main/java/com/example/bookStore/web/bookController.java
@@ -1,11 +1,15 @@
 package com.example.bookStore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookStore.domain.Book;
 import com.example.bookStore.domain.BookRepo;
@@ -52,6 +56,16 @@ public class bookController {
 	        return "edit";
 	    }     
 	
+	 
+	 @RequestMapping(value="/books", method = RequestMethod.GET)
+	 public @ResponseBody List<Book> bookListRest() {
+		 return (List<Book>) brepo.findAll();
+	 }
+	 
+	 @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	 public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookid) {
+		 return brepo.findById(bookid);
+	 }
 	
 	
 	
